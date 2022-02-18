3ae9eff2b802ee973d15b1073d3deaa862a93eaf
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-02-18 21:46:07 +0530

    Time: 1 ms (53.74%), Space: 46.3 MB (6.54%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,18 +1,29 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] op=new int[n*2];
+        int[] oparr=new int[2*n];
+        int opc=0;
         int start=0;
         int mid=n;
-        
-        int counter=0;
-        for(int i=0;i<n;i++){
-            op[counter]=nums[start];
-            op[counter+1]=nums[mid];
-            counter+=2;
-            start++;
-            mid++;
+        while(opc<(2*n)){
+            oparr[opc++]=nums[start++];
+            oparr[opc++]=nums[mid++];
         }
-        return op;
+
+        return oparr;
+
+//         int[] op=new int[n*2];
+//         int start=0;
+//         int mid=n;
+        
+//         int counter=0;
+//         for(int i=0;i<n;i++){
+//             op[counter]=nums[start];
+//             op[counter+1]=nums[mid];
+//             counter+=2;
+//             start++;
+//             mid++;
+//         }
+//         return op;
         
     }
 }

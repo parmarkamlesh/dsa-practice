097d3cfa3f1dfd63bbbb1dc0f43591128bf49560
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-02-21 16:21:52 +0530

    Time: 3 ms (76.24%), Space: 44.3 MB (9.32%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -3,25 +3,17 @@ class Solution {
     
 		ArrayList<Integer> alt = new ArrayList();
 		HashMap<Integer, Integer> n1map = new HashMap();
-		HashMap<Integer, Integer> n2map = new HashMap();
 
-		for (int i: nums1) {
-			n1map.put(i, n1map.getOrDefault(i, 0) + 1);
+        for (int i: nums1) {
+			n1map.put(i,1);
 		}
-
-		for (int i: nums2) {
-			n2map.put(i, n2map.getOrDefault(i, 0) + 1);
-		}
-		for (Integer a: n1map.keySet()) {
-			
-            if (n1map.containsKey(a) && n2map.containsKey(a)) {
-
-                if (n1map.get(a) > 0 && n2map.get(a) > 0) {
-						alt.add(a);
-				}
-
+        
+        for(Integer x:nums2){
+            if(n1map.getOrDefault(x,0)==1){
+                alt.add(x);
+                n1map.put(x,0);
             }
-		}
+        }
 
         int[] oparray = new int[alt.size()];
 		

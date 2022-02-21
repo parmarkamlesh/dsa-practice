869a6acf30213a89bb8804495c11912bdce1ae66
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-02-21 21:36:35 +0530

    Time: 2 ms (82.16%), Space: 45.6 MB (76.06%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,27 +1,29 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        if(nums.length==1)
-            return nums[0];
-        Arrays.sort(nums);
+    public int majorityElement(int[] a) {
+         Arrays.sort(a);
+       return a[a.length/2];
+//         if(nums.length==1)
+//             return nums[0];
+//         Arrays.sort(nums);
 
-        int howManyTime=nums.length/2;
-        howManyTime+=1;
+//         int howManyTime=nums.length/2;
+//         howManyTime+=1;
         
-        int counter=1;
+//         int counter=1;
         
-        for(int i=0;i<nums.length-1;i++){
+//         for(int i=0;i<nums.length-1;i++){
         
-            if(nums[i]==nums[i+1]){
-                counter++;
-                if(counter>=howManyTime){
-                    return nums[i];
-                }
-            }else{
-                counter=1;
-            }
+//             if(nums[i]==nums[i+1]){
+//                 counter++;
+//                 if(counter>=howManyTime){
+//                     return nums[i];
+//                 }
+//             }else{
+//                 counter=1;
+//             }
 
-        }
-        return 1;
+//         }
+//         return 1;
  
     }
 }

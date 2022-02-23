913b89de84613061d34b6034b61aa8a19b48ec90
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-02-23 20:51:45 +0530

    Time: 14 ms (28.40%), Space: 42.6 MB (71.96%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -4,14 +4,19 @@ class Solution {
     for (int i=0; i<9; ++i) {
         for (int j=0; j<9; ++j) {
             char number = board[i][j];
-            if (number != '.')
-                if (!seen.add(number + " in row " + i) ||
-                    !seen.add(number + " in column " + j) ||
-                    !seen.add(number + " in block " + i/3 + "-" + j/3))
-                    return false;
+            if (number != '.'){
+                if (!seen.add(number + "IR" + i) ||
+                    !seen.add(number + "IC" + j) ||
+                    !seen.add(number + "IB" + i/3 + "-" + j/3))
+                    return false;                
+            }
+                // if (!seen.add(number + " in row " + i) ||
+                //     !seen.add(number + " in column " + j) ||
+                //     !seen.add(number + " in block " + i/3 + "-" + j/3))
+                //     return false;
         }
     }
-        System.out.println(Arrays.asList(seen));
+//        System.out.println(Arrays.asList(seen));
     return true;
     }
 }

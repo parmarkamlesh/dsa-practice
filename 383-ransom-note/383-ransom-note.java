4aa28058eb6d9bfff0a4b6be19bf3996374b2f5e
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-02-24 21:53:59 +0530

    Time: 4 ms (76.83%), Space: 46.2 MB (39.28%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        // if(randsomNode.length()>magazine.length())
-        //     return false;
-        // if(randsomNode.length()==0 || magazine.length()==0)
-        //      return false;
+        if(ransomNote.length()>magazine.length())
+            return false;
+        if(ransomNote.length()==0 || magazine.length()==0)
+             return false;
         int[] chars=new int[26];
         for(int i=0;i<magazine.length();i++){
             chars[magazine.charAt(i)-'a']++;

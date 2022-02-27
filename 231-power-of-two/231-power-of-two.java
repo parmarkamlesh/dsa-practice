162e557e410b4d305f3c3e832369c265edae064a
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-02-27 08:41:14 +0530

    Time: 2 ms (63.06%), Space: 41.5 MB (19.65%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -4,5 +4,9 @@ class Solution {
             return true;
         else
             return n>=2 && n%2==0 && isPowerOfTwo(n/2);   
+      // if(n==0)
+      //   return false;
+      // return  (n & (n-1)) ==0;
+            
     }
 }

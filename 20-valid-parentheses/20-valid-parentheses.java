98bbfb30d2d53f842c8ee551418a76af6fd40d53
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-03-13 08:56:24 +0530

    Time: 4 ms (29.54%), Space: 41.7 MB (50.88%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,39 +1,39 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-            for (char c : s.toCharArray()) {
-                if (c == '(')
-                    stack.push(')');
-                else if (c == '{')
-                    stack.push('}');
-                else if (c == '[')
-                    stack.push(']');
-                else if (stack.isEmpty() || stack.pop() != c)
-                    return false;
-            }
-            return stack.isEmpty();        
-
-        // Stack<Character> stk=new Stack();
-        // for(int i=0;i<s.length();i++){
-        //     if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
-        //         stk.push(s.charAt(i));
-        //     }else{
-        //         if(stk.size()==0){
-        //             stk.push(s.charAt(i));                    
-        //             continue;
-        //         }
-        //         if(stk.peek()=='(' && s.charAt(i)==')'){
-        //             stk.pop();
-        //         }else if(stk.peek()=='{' && s.charAt(i)=='}'){
-        //             stk.pop();
-        //         }else if(stk.peek()=='[' && s.charAt(i)==']'){
-        //             stk.pop();                    
-        //         }else{
-        //             stk.push(s.charAt(i));                    
-        //         }
+        // Stack<Character> stack = new Stack<Character>();
+        //     for (char c : s.toCharArray()) {
+        //         if (c == '(')
+        //             stack.push(')');
+        //         else if (c == '{')
+        //             stack.push('}');
+        //         else if (c == '[')
+        //             stack.push(']');
+        //         else if (stack.isEmpty() || stack.pop() != c)
+        //             return false;
         //     }
-        // }
-        // return stk.size()==0;
+        //     return stack.isEmpty();        
+
+        Stack<Character> stk=new Stack();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                stk.push(s.charAt(i));
+            }else{
+                if(stk.size()==0){
+                    stk.push(s.charAt(i));                    
+                    continue;
+                }
+                if(stk.peek()=='(' && s.charAt(i)==')'){
+                    stk.pop();
+                }else if(stk.peek()=='{' && s.charAt(i)=='}'){
+                    stk.pop();
+                }else if(stk.peek()=='[' && s.charAt(i)==']'){
+                    stk.pop();                    
+                }else{
+                    stk.push(s.charAt(i));                    
+                }
+            }
+        }
+        return stk.size()==0;
         
     }
 }

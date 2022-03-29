7f82f63bfd1db0509e0e450700f8783c1f10e545
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-03-29 11:00:04 +0530

    Time: 43 ms (18.04%), Space: 75.3 MB (69.22%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,27 +1,27 @@
 class Solution {
     public int findDuplicate(int[] nums) {
 
-        int[] temparr=new int[nums.length+1];
+//         int[] temparr=new int[nums.length+1];
         
-        for(int x : nums){
-            temparr[x]++;
-        }
-        for(int i=0;i<temparr.length;i++){
-            if(temparr[i]>1)
-                return i;
-        }
-        return -1;        
+//         for(int x : nums){
+//             temparr[x]++;
+//         }
+//         for(int i=0;i<temparr.length;i++){
+//             if(temparr[i]>1)
+//                 return i;
+//         }
+//         return -1;        
         
         
         //method 2
-        // Arrays.sort(nums);
-        // // System.out.println(Arrays.toString(nums));
-        // //no need to check index out of bound because
-        // //problem guarnteed atleast 1 duplicate
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==nums[i+1])
-        //         return nums[i];
-        // }
-        // return -1;
+        Arrays.sort(nums);
+        // System.out.println(Arrays.toString(nums));
+        //no need to check index out of bound because
+        //problem guarnteed atleast 1 duplicate
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==nums[i+1])
+                return nums[i];
+        }
+        return -1;
     }
 }

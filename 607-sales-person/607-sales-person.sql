fbf563952c480c8699e743ccc4319ed5f7dcdf39
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-04-15 21:29:47 +0530

    Time: 1528 ms (21.70%), Space: 0B (100.00%) - LeetHub

---
+++ 607-sales-person/607-sales-person.sql
@@ -0,0 +1,8 @@
+#todo
+SELECT s.NAME 
+FROM   salesperson s 
+WHERE  sales_id NOT IN(SELECT sales_id 
+                       FROM   orders o 
+                              LEFT JOIN company c 
+                                     ON o.com_id = c.com_id 
+                       WHERE  c.NAME = 'RED') 

a2484c9cc83a84e72ea05184295519bfa8a8254c
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-05-07 22:38:22 +0530

    Update 35-search-insert-position.java

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,11 +1,9 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int left = 0, right =nums.length-1;
-
         while (left <= right) {
-        
-            int mid = left + (right - left) / 2;
-            
+       
+            int mid = left + (right - left) / 2;        
             if (nums[mid]==target){
                 return mid;
             }

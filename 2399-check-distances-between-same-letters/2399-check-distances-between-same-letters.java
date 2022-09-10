a243f656d276d1c7db26ef81ba52294f9f07b21b
parmar kamlesh <31653703+parmarkamlesh@users.noreply.github.com>
2022-09-10 23:13:48 +0530

    Time: 3 ms (48.93%), Space: 43.5 MB (38.95%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.java
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.java
@@ -3,15 +3,18 @@ class Solution {
         HashMap<Character,Integer> hmap=new HashMap<>();
         for(int i =0;i<s.length();i++){
             if(hmap.containsKey(s.charAt(i))){
-                hmap.put(s.charAt(i),(i+1)-hmap.get(s.charAt(i))-1);
+                // hmap.put(s.charAt(i),(i+1)-hmap.get(s.charAt(i))-1);
+                if(distance[s.charAt(i)-97]!=i-hmap.get(s.charAt(i))){
+                    return false;
+                }
             }else{
                 hmap.put(s.charAt(i),i+1);
             }
         }
-    for (Map.Entry<Character,Integer> entry : hmap.entrySet()) 
-            if(distance[entry.getKey()-97]!=entry.getValue()){
-                return false;
-            }
+    // for (Map.Entry<Character,Integer> entry : hmap.entrySet()) 
+    //         if(distance[entry.getKey()-97]!=entry.getValue()){
+    //             return false;
+    //         }
         return true;
     }
 }

e3a67c27779e185234cee10cb6a2a863afba1b04
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2022-11-03 23:08:24 +0530

    Time: 2 ms (81.34%), Space: 46.1 MB (32.51%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,14 +1,29 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-     Queue<Integer> q = new LinkedList<>();
+        int countZero = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) countZero++;
+        }
+        int len = arr.length + countZero;
+        //todo 
+        for (int i = arr.length - 1, j = len - 1; i < j; i--, j--) {
+            if (arr[i] != 0) {
+                if (j < arr.length) arr[j] = arr[i];
+            } else {
+                if (j < arr.length) arr[j] = arr[i];
+                j--;
+                if (j < arr.length) arr[j] = arr[i]; 
+            }
+        }
+        //      Queue<Integer> q = new LinkedList<>();
 
-        for(int i=0; i<arr.length; i++){
-            q.add(arr[i]);
+//         for(int i=0; i<arr.length; i++){
+//             q.add(arr[i]);
 
-            if(arr[i] == 0)
-                q.add(0);
+//             if(arr[i] == 0)
+//                 q.add(0);
 
-            arr[i] = q.remove();
-        }     
+//             arr[i] = q.remove();
+//         }     
     }
 }

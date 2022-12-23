819cd9f83eb2052d98c8c3d21baa464791594fc0
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2022-12-23 21:40:45 +0530

    Time: 0 ms (100.00%), Space: 43 MB (61.43%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int len = bits.length;
+        int count = 0;
+        for (int i = len - 2; i >= 0; i--) {
+            if (bits[i] == 1) count++;
+            else break;
+        }
+        return count%2 == 0;    
+    }
+}

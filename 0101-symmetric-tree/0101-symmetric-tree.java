52df8116e37b0828b02b858498bf02425a67f767
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-03-13 23:29:00 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (25.47%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         if(root==null){
+//             return false;
+//         }
+        
+//         Stack<TreeNode> stk=new Stack();
+
+//         stk.push(root.left);
+//         stk.push(root.right);
+        
+//         while(!stk.empty()){
+//             TreeNode one=stk.pop();
+//             TreeNode two=stk.pop();
+            
+//             //if both null means its leaf
+//             if(one == null && two == null){
+//                 continue;
+//             }
+//             //if anyone of null or value is not match means its not mirror
+//             if(one == null || two == null || one.val != two.val){
+//                 return false;
+//             }
+            
+//             //add all leaf node in stack
+//             stk.push(one.left);
+//             stk.push(two.right);            
+//             stk.push(one.right);
+//             stk.push(two.left);
+//         }
+        
+//         return true;
+//     }
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isMirror(root.left,root.right);
+    }
+    public boolean isMirror(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        return (p.val == q.val) && isMirror(p.left,q.right) && isMirror(p.right,q.left);
+    }
+}

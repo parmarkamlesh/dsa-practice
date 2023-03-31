fe0ca225d57c49a4d8f815dbcacef2021ff00d0e
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-03-31 23:52:14 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (54.23%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int passThePillow(int n, int t) {
+        // TODO
+        int rounds = t/(n-1);
+        int k = t % (n-1);
+        return rounds % 2 == 0 ? k+1 : n - k;
+    }
+}

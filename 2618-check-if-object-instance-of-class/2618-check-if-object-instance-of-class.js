bf557db723a4974107cb61e192d603fb8b814e34
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-04-15 09:24:57 +0530

    Time: 103 ms (78.03%), Space: 50.7 MB (99.78%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Object} object
+ * @param {Function} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    
+    const prototype = classFunction?.prototype;
+    
+    while (obj !== null && obj !== undefined) {
+        obj = Object.getPrototypeOf(obj);
+        if (obj === prototype) return true;
+    }
+    
+    return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */

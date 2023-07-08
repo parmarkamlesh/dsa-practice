4051bba93b40bf546f783b98b142876a66235121
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-07-08 21:29:31 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (79.88%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -3,17 +3,17 @@ class Solution {
         String[] s=sentence.split(" ");
         int counter=0;
         for(String i:s){
-        if(i.length()>=searchWord.length()){
-            int flag=0;
-            for (int j = 0; j < searchWord.length(); j++) {
-                if(searchWord.charAt(j)!=i.charAt(j))
-                {flag=1;break;}
+            if(i.length()>=searchWord.length()){
+                int flag=0;
+                for (int j = 0; j < searchWord.length(); j++) {
+                    if(searchWord.charAt(j)!=i.charAt(j))
+                    {flag=1;break;}
+                }
+                if(flag==0){
+                    return counter+1;
+                }
             }
-            if(flag==0){
-                return counter+1;
-            }
-        }
-        counter++;
+            counter++;
 
         }
         return -1;

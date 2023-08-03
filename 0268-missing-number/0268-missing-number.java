bc383ab1908b7c020725318b0d13840af748c444
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-08-03 22:30:26 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (96.17%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // return (((nums.length+1)*nums.length)/2)-Arrays.stream(nums).sum();
+        int sm=0;
+        for(int i:nums) sm+=i;
+        return (nums.length+1)*nums.length/2 - sm;
+       
+    }
+}

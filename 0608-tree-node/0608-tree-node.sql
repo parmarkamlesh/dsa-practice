be526df16c7bc98ec7fb1d0159a7289253e07b0d
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-08-12 04:28:22 +0530

    Update 0608-tree-node.sql
    Wrong column name

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -2,4 +2,4 @@
 SELECT atree.id,
     IF(ISNULL(atree.p_id),
         'Root',
-        IF(atree.id IN (SELECT p_id FROM tree), 'Inner','Leaf')) Type FROM tree atree ORDER BY atree.id
+        IF(atree.id IN (SELECT p_id FROM tree), 'Inner','Leaf')) type FROM tree atree ORDER BY atree.id

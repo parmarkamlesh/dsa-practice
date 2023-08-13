ea85d4a1d806dc6d44c212872afede4f906b9376
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-08-13 23:16:51 +0530

    Update 0083-remove-duplicates-from-sorted-list.java
    Old code rem

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -29,11 +29,5 @@ class Solution {
 
         }
         return head;
-//         if(head == null || head.next == null)
-//             return head;
-        
-//         head.next = deleteDuplicates(head.next);
-        
-//         return head.val == head.next.val ? head.next : head;        
-    }
+}
 }

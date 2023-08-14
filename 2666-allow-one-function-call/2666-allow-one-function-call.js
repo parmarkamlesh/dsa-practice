d65a5c0323657c3138cc37d8fddbdb275bba564c
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-08-14 23:25:37 +0530

    Update 2666-allow-one-function-call.js

--- 2666-allow-one-function-call/2666-allow-one-function-call.js
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -1,7 +1,3 @@
-/**
- * @param {Function} fn
- * @return {Function}
- */
 var once = function(fn) {
       let hasBeenCalled = false;
       let result;
@@ -16,11 +12,3 @@ var once = function(fn) {
         }
       };
 };
-
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */

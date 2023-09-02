c5cdd6b3b9fb0b87082d92512fe3d304c47b94d2
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-09-02 22:43:06 +0530

    Update 2169-count-operations-to-obtain-zero.java
    update algo

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int countOperations(int num1, int num2) {
+    public int countOperations(int num1, int num2) {/
+        // return a * b == 0 ? 0 : a / b + countOperations(b, a % b);
         int counter=0;
         if(num1==0 || num2==0){
             return 0;

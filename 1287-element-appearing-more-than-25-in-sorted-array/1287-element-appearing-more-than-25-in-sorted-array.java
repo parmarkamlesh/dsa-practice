6196c2fe6a5b73af69ab528d6c22c874fb69e616
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-12-11 23:07:49 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (94.00%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -1,17 +1,29 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
-        Map<Integer, Integer> counts = new HashMap();
-        for (int num : arr) {
-            counts.put(num, counts.getOrDefault(num, 0) + 1);
-        }
-        
-        int target = arr.length / 4;
-        for (int key : counts.keySet()) {
-            if (counts.get(key) > target) {
-                return key;
+        int size = arr.length / 4;
+        for (int i = 0; i < arr.length - size; i++) {
+            if (arr[i] == arr[i + size]) {
+                return arr[i];
             }
         }
         
         return -1;
     }
 }
+// class Solution {
+//     public int findSpecialInteger(int[] arr) {
+//         Map<Integer, Integer> counts = new HashMap();
+//         for (int num : arr) {
+//             counts.put(num, counts.getOrDefault(num, 0) + 1);
+//         }
+        
+//         int target = arr.length / 4;
+//         for (int key : counts.keySet()) {
+//             if (counts.get(key) > target) {
+//                 return key;
+//             }
+//         }
+        
+//         return -1;
+//     }
+// }

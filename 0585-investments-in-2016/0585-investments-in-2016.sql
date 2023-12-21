581e4d7d44dd7a00d9a8383675badc1c29e5d08d
Kamlesh Parmar <31653703+parmarkamlesh@users.noreply.github.com>
2023-12-21 22:18:13 +0530

    Time: 1594 ms (5.32%), Space: 0B (100.00%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,9 @@
+SELECT ROUND(SUM(tiv_2016), 2) tiv_2016
+FROM Insurance a
+WHERE a.tiv_2015 IN (SELECT tiv_2015
+                     FROM Insurance
+                     WHERE pid <> a.pid)
+  AND (a.lat, a.lon) NOT IN
+      (SELECT lat, lon
+       FROM Insurance
+       WHERE pid <> a.pid);
